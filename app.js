3bbd982b236392d5af183013bb2945004247deac
Brandon Holifield <brandonholifield31@gmail.com>
2025-11-07 17:47:11 -0800

    Actions error

--- app.js
+++ app.js
@@ -8,6 +8,19 @@ const mainRouter = require("./routes/index");
 const app = express();
 const { PORT = 3001 } = process.env;
 
+function authorizeUser(req, res, next) {
+  req.user = {
+    _id: "5d8b8592978f8bd833ca8133",
+  };
+  console.log("Mock user ID set:", req.user._id);
+  next();
+}
+
+app.use(express.json());
+app.use(cors());
+app.use(authorizeUser);
+app.use("/", mainRouter);
+
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
   .then(() => {
@@ -17,7 +30,3 @@ mongoose
     });
   })
   .catch(console.error);
-
-app.use(express.json());
-app.use(cors());
-app.use("/", mainRouter);
